c2e5e63131772b7cf2a894ee1bc549f29d3355fa
briguy202 <brian.hibma@amway.com>
2014-02-23 22:21:49 -0500

    Adding WSFed

---
+++ PrototypeApplication.MVC.WSFed/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PrototypeApplication.MVC.WSFed.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			ViewBag.Title = "Home Page";
+
+			return View();
+		}
+	}
+}
---
+++ PrototypeApplication.MVC.WSFed/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PrototypeApplication.MVC.WSFed.WebApiApplication" Language="C#" %>
