6405720753892162750452faa28f97b49adb3b34
briguy202 <brian.hibma@amway.com>
2014-02-23 20:49:34 -0500

    MVC Checkpoint

---
+++ PrototypeApplication.MVC/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PrototypeApplication.MVC.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			ViewBag.Title = "Home Page";
+
+			return View();
+		}
+
+		[Authorize]
+		public ActionResult Identity()
+		{
+			return View(ClaimsPrincipal.Current);
+		}
+	}
+}
---
+++ PrototypeApplication.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PrototypeApplication.MVC.WebApiApplication" Language="C#" %>
